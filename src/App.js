1af9840a615a36181441afa6e312b053fb600960
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2024-12-16 06:36:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+<h1 className='text-red-600'>Expense Tracker</h1>
+
+    </div>
+     );
+}
+
+export default App;
