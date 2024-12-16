0a9f16804c63d92e31238126a7ddd71fa8c3aac5
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2024-12-16 07:06:45 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
+import Graph from './components/Graph';
 
 function App() {
   return (
-    <div>
-<h1 className='text-red-600'>Expense Tracker</h1>
-
+    <div className="App">
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className='text-4xl py-8 mb-10 bg-slate-800 text-white rounded '>Expense Tracker</h1>
+        <div className='grid md:grid-cols-2 gap-4'>
+<Graph></Graph>
+        </div>
+      </div>
     </div>
-     );
+  );
 }
 
 export default App;
---
+++ src/components/Graph.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Graph = () => {
+  return (
+    <div className='flex justify-content max-w-xs mx-auto'>
+    
+    </div>
+  )
+}
+
+export default Graph
